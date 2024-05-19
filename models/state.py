930d69a3f1d1d231c903af5bd9aa0de0ad536fa1
jaskieerr <nizar.zairat@gmail.com>
2024-05-19 06:49:44 -0700

    addin state from justin

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the state class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import environ
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    '''hbnb clone state class'''
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        @property
+        def cities(self):
+            '''cities getter'''
+            from models import storage
+            from models.city import City
+            city_dicts = storage.all(City)
+            city_list = []
+
+            for c in city_dicts.values():
+                if c.state_id == self.id:
+                    city_list.append(c)
+
+            return city_list
